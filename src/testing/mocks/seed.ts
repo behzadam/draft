64d7ce34795855be23bec5f0d6db40f4798b41fb
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2023-04-20 21:36:59 +0330

    chore: add msw db seeder

---
+++ src/testing/mocks/seed.ts
@@ -0,0 +1,10 @@
+import * as fakeData from './data';
+import { db } from './db';
+
+export const seed = () => {
+  const postCount = db.post.count();
+  if (postCount > 0) return;
+
+  fakeData.users.forEach((user) => db.user.create(user));
+  fakeData.posts.forEach((post) => db.post.create(post));
+};
