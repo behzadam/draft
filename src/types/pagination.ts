9ff5607385439919c5bab015dce1f69b47a9022a
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2023-04-21 16:20:14 +0330

    chore: add pagination type

---
+++ src/types/pagination.ts
@@ -0,0 +1,25 @@
+export class Pagination {
+  page: number;
+  take: number;
+  itemCount: number;
+  pageCount: number;
+  hasPreviousPage: boolean;
+  hasNextPage: boolean;
+
+  constructor({
+    itemCount,
+    page,
+    take
+  }: {
+    page: number;
+    take: number;
+    itemCount: number;
+  }) {
+    this.page = page;
+    this.take = take;
+    this.itemCount = itemCount;
+    this.pageCount = Math.ceil(this.itemCount / this.take);
+    this.hasPreviousPage = this.page > 1;
+    this.hasNextPage = this.page < this.pageCount;
+  }
+}
