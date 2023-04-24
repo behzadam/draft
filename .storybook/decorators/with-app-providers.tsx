b1286c3e33cba6c588e09c5a6bc30deabfb47eb0
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2023-04-24 14:39:10 +0330

    chore: add StoryFn

--- .storybook/decorators/with-app-providers.tsx
+++ .storybook/decorators/with-app-providers.tsx
@@ -1,8 +1,10 @@
-import type { Decorator } from '@storybook/react';
+import type { Decorator, StoryFn } from '@storybook/react';
 import React from 'react';
 import { AppProviders } from '../../src/app/providers';
 
-export const withAppProviders: Decorator = (Story) => (
+export const withAppProviders: Decorator = (
+  Story: StoryFn
+) => (
   <AppProviders>
     <Story />
   </AppProviders>
