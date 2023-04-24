f7e60484fa90101de5f71485a9816f46033df5d4
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2023-04-24 12:37:17 +0330

    chore: add decorators

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,12 @@
 import type { Preview } from '@storybook/react';
+import {
+  initialize,
+  mswDecorator
+} from 'msw-storybook-addon';
+import '../src/app/globals.css';
+import { withAppProviders } from './decorators';
+
+initialize();
 
 const preview: Preview = {
   parameters: {
@@ -8,8 +16,12 @@ const preview: Preview = {
         color: /(background|color)$/i,
         date: /Date$/
       }
+    },
+    nextjs: {
+      appDirectory: true
     }
-  }
+  },
+  decorators: [mswDecorator, withAppProviders]
 };
 
 export default preview;
