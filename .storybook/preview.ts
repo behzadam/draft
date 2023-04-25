1a45ce58a4d9b396ed7510e5080ec984a9dd63dd
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2023-04-25 17:44:27 +0330

    chore: initial msw

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,12 +1,15 @@
 import type { Preview } from '@storybook/react';
 import {
-  initialize,
-  mswDecorator
+  mswDecorator,
+  initialize as mswInitialize
 } from 'msw-storybook-addon';
 import '../src/app/globals.css';
+import { handlers } from '../src/testing/mocks/handlers';
+import { seedMockDb } from '../src/testing/mocks/seeder';
 import { withAppProviders } from './decorators';
 
-initialize();
+mswInitialize();
+seedMockDb();
 
 const preview: Preview = {
   parameters: {
@@ -21,7 +24,13 @@ const preview: Preview = {
       appDirectory: true
     }
   },
-  decorators: [mswDecorator, withAppProviders]
+  decorators: [withAppProviders, mswDecorator]
+};
+
+preview.parameters = {
+  msw: {
+    handlers: handlers
+  }
 };
 
 export default preview;
