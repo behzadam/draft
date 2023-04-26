66386742961a2259a5ba00f6adf9b99f87f03f9b
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2023-04-26 13:46:24 +0330

    feat: add header

--- src/features/site/post/components/post-list.tsx
+++ src/features/site/post/components/post-list.tsx
@@ -9,7 +9,8 @@ export const PostList = ({
   posts
 }: Props): JSX.Element => {
   return (
-    <section className="grid grid-cols-1 divide-y">
+    <section className="grid max-w-md grid-cols-1 mx-auto divide-y divide-gray-100">
+      <h1 className="py-6 text-xl font-bold">Posts</h1>
       {posts.map((post) => {
         return <PostListItem post={post} key={post.id} />;
       })}
