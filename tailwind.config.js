a994f805fa6d879a9b0048118104ef70ee1246f3
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2023-04-26 13:16:39 +0330

    chore: remove global `a` underline

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,15 @@ module.exports = {
           'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+      },
+      typography: {
+        DEFAULT: {
+          css: {
+            a: {
+              textDecoration: 'none'
+            }
+          }
+        }
       }
     }
   },
