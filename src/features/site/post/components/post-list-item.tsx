3cf6097fa7a815a4b787c3ed4e3187bb79ac2622
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2023-04-26 13:48:01 +0330

    refactor: remove Link component

--- src/features/site/post/components/post-list-item.tsx
+++ src/features/site/post/components/post-list-item.tsx
@@ -1,4 +1,4 @@
-import { Link } from '@/components/link';
+import Link from 'next/link';
 import { Post } from '../types/post';
 
 type Props = {
@@ -9,11 +9,11 @@ export const PostListItem = ({
   post
 }: Props): JSX.Element => {
   return (
-    <article>
-      <h3 className="text-2xl font-bold leading-8 tracking-tight">
+    <article className="py-4">
+      <h3>
         <Link href="#">{post.title}</Link>
       </h3>
-      <div className="prose text-gray-500 max-w-none dark:text-gray-400">
+      <div className="text-xs text-gray-600">
         {post.createdAt}
       </div>
     </article>
