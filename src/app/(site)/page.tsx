3060a5f97935e3a3cfeef5256a099eebe41ed7da
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2023-04-28 18:53:44 +0330

    feat: add skeleton

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -2,11 +2,20 @@
 
 import {
   PostList,
+  PostListSkeleton,
   useGetPosts
 } from '@/features/site/post';
 
 export default function Site() {
-  const { result, isLoading } = useGetPosts();
-  if (isLoading) return <p>Loading...</p>;
-  return <PostList posts={result.data!!} />;
+  const { result, isFetching } = useGetPosts();
+  return (
+    <section className="grid max-w-md grid-cols-1 mx-auto divide-y divide-gray-100">
+      <h1 className="py-6 text-xl font-bold">Posts</h1>
+      {isFetching ? (
+        <PostListSkeleton />
+      ) : (
+        <PostList posts={result.data} />
+      )}
+    </section>
+  );
 }
