bbf927ec127be65a990db90d8a57c309e23b50a6
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2023-04-28 18:25:30 +0330

    feat: export `post-list.skeleton`

--- src/features/site/post/index.ts
+++ src/features/site/post/index.ts
@@ -1,9 +1,10 @@
 // api
 export * from './api/get-post-details';
 export * from './api/get-posts';
+// components
 export * from './components/list/post-list';
 export * from './components/list/post-list-item';
-// components
+export * from './components/list/post-list.skeleton';
 export * from './components/post-details';
 // types
 export * from './types/post';
