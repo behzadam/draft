4d48df0446c14cb0c8a8fd1ea559956d1b419979
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2023-05-01 14:49:06 +0330

    chore: change story navigation to `List`

--- src/features/site/post/components/list/post-list.skeleton.stories.tsx
+++ src/features/site/post/components/list/post-list.skeleton.stories.tsx
@@ -1,11 +1,11 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { PostListSkeleton } from './post-list.skeleton';
+import { PostListSkeleton as Skeleton } from './post-list.skeleton';
 
 const meta = {
-  title: 'Features/Post',
-  component: PostListSkeleton
-} satisfies Meta<typeof PostListSkeleton>;
+  title: 'Features/Post/List',
+  component: Skeleton
+} satisfies Meta<typeof Skeleton>;
 export default meta;
 
 type Story = StoryObj<typeof meta>;
-export const Skeleton: Story = {};
+export const PostListSkeleton: Story = {};
