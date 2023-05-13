9a5c8464133b2bccb964102cf0bed6492fe7732c
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2023-05-13 17:19:15 +0330

    chore: replace AppProvider with QueryClientProvider

--- .storybook/decorators/with-app-providers.tsx
+++ .storybook/decorators/with-app-providers.tsx
@@ -1,11 +1,17 @@
 import type { Decorator, StoryFn } from '@storybook/react';
+import {
+  QueryClient,
+  QueryClientProvider
+} from '@tanstack/react-query';
 import React from 'react';
-import { AppProviders } from '../../src/app/providers';
+import { reactQueryOptions } from '../../src/lib/react-query';
 
 export const withAppProviders: Decorator = (
   Story: StoryFn
 ) => (
-  <AppProviders>
+  <QueryClientProvider
+    client={new QueryClient(reactQueryOptions)}
+  >
     <Story />
-  </AppProviders>
+  </QueryClientProvider>
 );
