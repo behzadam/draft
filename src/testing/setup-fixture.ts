0bd999da6da0a1b474dd0a6a9f83d98d991e62c7
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2023-05-13 18:15:25 +0330

    test: fix queryClient.clear

--- src/testing/setup-fixture.ts
+++ src/testing/setup-fixture.ts
@@ -1,19 +1,22 @@
 import '@testing-library/jest-dom';
 import '@testing-library/jest-dom/extend-expect';
 
-import { getQueryClient } from '@/lib/react-query';
 import { seedDatabase } from '@/testing/database/seeder';
 import { server } from '@/testing/mocks/server';
+import { QueryClient } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 beforeAll(() => {
   server.listen({ onUnhandledRequest: 'error' });
   seedDatabase();
 });
 
-afterAll(() => server.close());
-
 afterEach(async () => {
-  const queryClient = getQueryClient();
-  queryClient.clear();
   server.resetHandlers();
+});
+
+afterAll(() => {
+  server.close();
+  queryClient.clear();
 });
