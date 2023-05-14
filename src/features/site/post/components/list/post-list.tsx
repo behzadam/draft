b107cce67e34dfe3db62be43abfeeb7d7ae2684c
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2023-05-14 20:20:20 +0330

    feat: add post list heading

--- src/features/site/post/components/list/post-list.tsx
+++ src/features/site/post/components/list/post-list.tsx
@@ -1,23 +1,40 @@
 'use client';
 
+import { Lato } from 'next/font/google';
 import { useGetPosts } from '../../api/get-posts';
 import { PostListItem } from './post-list-item';
 import { PostListSkeleton } from './post-list-skeleton';
 
+export const subHeadingFont = Lato({
+  subsets: ['latin'],
+  weight: '300'
+});
+
 export const PostList = (): JSX.Element => {
   const { result, isFetching } = useGetPosts();
   return (
-    <section className="grid max-w-md grid-cols-1 mx-auto divide-y divide-gray-100">
-      <h1 className="py-6 text-xl font-bold">Posts</h1>
-      {isFetching ? (
-        <PostListSkeleton />
-      ) : (
-        result?.data?.map((post) => {
-          return (
-            <PostListItem post={post} key={post.id} />
-          );
-        })
-      )}
+    <section className="container grid max-w-screen-lg grid-cols-1">
+      <header className="py-8 text-center">
+        <p
+          className={`${subHeadingFont.className} py-3 text-sm`}
+        >
+          Front-End Next.js Architecture
+        </p>
+        <h1 className="mx-auto text-5xl font-black">
+          Draft Blog Posts
+        </h1>
+      </header>
+      <section className="divide-y divide-gray-100">
+        {isFetching ? (
+          <PostListSkeleton />
+        ) : (
+          result?.data?.map((post) => {
+            return (
+              <PostListItem post={post} key={post.id} />
+            );
+          })
+        )}
+      </section>
     </section>
   );
 };
