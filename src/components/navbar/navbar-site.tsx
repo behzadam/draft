d8069d81113ae033259c9adeb708118fb78ce0d7
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2023-05-14 19:35:01 +0330

    feat: add NavbarSite component

---
+++ src/components/navbar/navbar-site.tsx
@@ -0,0 +1,12 @@
+export const NavbarSite = () => {
+  return (
+    <nav className="text-sm">
+      <a
+        href="https://github.com/behzadam/draft"
+        target="_blank"
+      >
+        GitHub
+      </a>
+    </nav>
+  );
+};
